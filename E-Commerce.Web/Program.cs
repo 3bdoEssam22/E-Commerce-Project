47585d649a4b099bb487ec98728d62da9d408dc1
3bdoEssam22 <abdulrahman.e.f22@gmail.com>
2025-04-14 02:36:09 +0300

    Part 06 Products-Brands-Types Data Seeding

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,5 +1,7 @@
 
+using DomainLayer.Contracts;
 using Microsoft.EntityFrameworkCore;
+using Persistence;
 using Persistence.Data;
 
 namespace E_Commerce.Web
@@ -17,11 +19,17 @@ namespace E_Commerce.Web
             builder.Services.AddSwaggerGen();
             builder.Services.AddDbContext<StoreDbContext>(options =>
                     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+            builder.Services.AddScoped<IDataSeeding, DataSeeding>();
 
             #endregion            
 
             var app = builder.Build();
+
+            using var scope = app.Services.CreateScope();
+
+            var ObjectOfDataSeeding = scope.ServiceProvider.GetRequiredService<IDataSeeding>();
+
+            ObjectOfDataSeeding.DataSeed();
 
             #region Configure the HTTP request pipeline.
 
