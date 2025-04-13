2f452bfce8ac9c2b106e364bbb163e8151cbfd18
3bdoEssam22 <abdulrahman.e.f22@gmail.com>
2025-04-14 01:32:04 +0300

    Part 05 Product Configurations & Db Context

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
 namespace E_Commerce.Web
 {
     public class Program
@@ -7,16 +10,21 @@ namespace E_Commerce.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            #region Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+            #endregion            
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            #region Configure the HTTP request pipeline.
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -25,10 +33,9 @@ namespace E_Commerce.Web
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
-
-
             app.MapControllers();
+
+            #endregion
 
             app.Run();
         }
