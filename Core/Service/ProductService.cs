33121f1a2f5b4ef042294a06f4411a8a142c4f99
3bdoEssam22 <abdulrahman.e.f22@gmail.com>
2025-04-16 22:51:05 +0300

    Part 05 Product Module - Product Service

---
+++ Core/Service/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using DomainLayer.Contracts;
+using DomainLayer.Models;
+using ServiceAbstracion;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ProductService(IUnitOfWork _unitOfWork, IMapper _mapper) : IProductService
+    {
+        public async Task<IEnumerable<BrandDTo>> GetAllBrandsAsync()
+        {
+            var Repo = _unitOfWork.GetRepository<ProductBrand, int>();
+            var Brands = await Repo.GetAllAsync();
+            var BrandsDto = _mapper.Map<IEnumerable<BrandDTo>>(Brands);
+            return BrandsDto;
+        }
+
+        public async Task<IEnumerable<ProductDTo>> GetAllProductsAsync()
+        {
+            var products = await _unitOfWork.GetRepository<Product, int>().GetAllAsync();
+            return _mapper.Map<IEnumerable<Product>, IEnumerable<ProductDTo>>(products);
+        }
+
+        public async Task<IEnumerable<TypeDTo>> GetAllTypesAsync()
+        {
+            var Types = await _unitOfWork.GetRepository<ProductType, int>().GetAllAsync();
+            return _mapper.Map<IEnumerable<ProductType>, IEnumerable<TypeDTo>>(Types);
+        }
+
+        public async Task<ProductDTo> GetProductByIdAsync(int id)
+        {
+            var product = await _unitOfWork.GetRepository<Product, int>().GetByIdAsync(id);
+            return _mapper.Map<Product, ProductDTo>(product);
+        }
+    }
+}
