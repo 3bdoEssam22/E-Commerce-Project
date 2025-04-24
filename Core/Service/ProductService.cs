e328107f58e0896ff71534e466fa5ff9ae1809cd
3bdoEssam22 <abdulrahman.e.f22@gmail.com>
2025-04-24 04:19:12 +0300

    Part 09 Basket Module - Entities

--- Core/Service/ProductService.cs
+++ Core/Service/ProductService.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
 using DomainLayer.Contracts;
 using DomainLayer.Exceptions;
-using DomainLayer.Models;
+using DomainLayer.Models.ProductModule;
 using Services.Specifications;
 using ServicesAbstracion;
 using Shared;
